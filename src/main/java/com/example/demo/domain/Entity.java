4898289301a0f9fa475c44ba7dc516709869d8d9
Liu Min <liumin5@le.com>
2017-10-16 22:12:34 +0800

    add entity user requestbody

---
+++ src/main/java/com/example/demo/domain/Entity.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+/**
+ * Created by liumin5 on 2017/10/16.
+ */
+public class Entity {
+
+    private String name;
+    private String password;
+
+    public Entity(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
